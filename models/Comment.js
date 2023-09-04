d87e93ffa68e1d1e341d0c0cbb132ced206c0528
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-03 21:04:52 -0500

    Created: 1. Relationship between models. 2. Models for Users, Posts, Comments

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        reference: {
+          model: "post",
+          key: "id",
+        },
+      },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,45 @@
+// Import models
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// User has many posts.
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+// User has many comments.
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+// Posts belong to one user. 
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+// Posts have many comments.
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+// Comments belong to one user.
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+// Comments belong to one post. 
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
