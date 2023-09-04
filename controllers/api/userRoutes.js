2bef37c3d0f0ae04af511781f31af3ecc54a8f55
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-03 22:59:55 -0500

    Created Routes for User Login and Signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,68 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// *** DELETE LATER - TEST ROUTE
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll(
+        // {include: [{ model: Post }, { model: Comment }],}
+        );
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// User Sign Up Route
+router.post("/", async (req, res) => {
+  try {
+    const validName = await User.findOne({ where: { name: req.body.name } });
+
+    // If username is taken, message is displayed prompting to choose another username.
+    if (validName) {
+      res
+        .status(400)
+        .json({ message: "Username is taken. Choose another username." });
+      return;
+    }
+
+    const userData = await User.create(req.body);
+
+    // Session Storage Placeholder
+
+    res.status(200).json({ user: userData, message: "Successfully created account!" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User Login Route
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { name: req.body.name } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    // Session Storage Placeholder
+
+    res.json({ user: userData, message: "You are now logged in!" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const path = require('path');
+// const path = require('path');
 
+// Imports Express.js.
 const express = require('express');
 
-// const routes = require('./controllers');
+// Import the routes.
+const routes = require('./controllers');
 
-const sequelize = require('./config/connections');
+// Import the connection object: Sequelize connection.
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// Middleware for parsing JSON and urlencoded form data.
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes)
+//Send all the requests that begin with / to the index.js in the routes folder.
+app.use(routes)
 
+// Synchronize sequelize models to the database before starting Express.js server, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening!'));
 });
