23e93605544d3d3d5973694d5195ce6f2a905581
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-04 01:32:36 -0500

    Created Dashboard Route: To display all of a user's blog posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,38 +1,7 @@
 const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
+const { Post } = require("../../models");
 
 // The `/api/posts` endpoint
-
-// Display all blog posts on homepage.
-router.get("/", async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      include: [{ model: User, attributes: ["name"] }],
-    });
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Display selected blog post and comments on post's page.
-router.get("/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        { model: User, attributes: ["name"] },
-        {
-          model: Comment,
-          attributes: ["content", "createdAt", "updatedAt"],
-          include: [{ model: User, attributes: ["name"] }],
-        },
-      ],
-    });
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Creates a new blog post.
 router.post("/", async (req, res) => {
