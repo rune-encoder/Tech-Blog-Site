f7f0b80056564666f553d78574febc46bd248ff8
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-04 20:38:00 -0500

    Testing Routes with Handlebars Engine

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-// const path = require('path');
+// Import node.js path module provides utilities for working with file and directory paths.
+const path = require('path');
 
 // Imports Express.js.
 const express = require('express');
+
+// Import express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 // Import the routes.
 const routes = require('./controllers');
@@ -9,14 +14,20 @@ const routes = require('./controllers');
 // Import the connection object: Sequelize connection.
 const sequelize = require('./config/connection');
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set Handlebars.js as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Middleware for parsing JSON and urlencoded form data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(express.static(path.join(__dirname, 'public')));
+// Middleware pointing to the public directory (absolute path).
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Send all the requests that begin with / to the index.js in the routes folder.
 app.use(routes)
