b042eb87b21a85b99237648c070266ebc681e548
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-09-08 08:03:43 -0500

    MVP. Created helps for date and time

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,7 @@
 module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'});
+  },
   // The custom helper 'format_date' takes in a timestamp
   format_date: (date) => {
     const formattedDate = new Date(date);
@@ -10,21 +13,3 @@ module.exports = {
   },
 };
 
-// const format_date = (date) => {
-//   const formattedDate = new Date(date);
-//   const year = formattedDate.getFullYear();
-//   const month = formattedDate.getMonth() + 1;
-//   const day = formattedDate.getDate();
-
-//   console.log(`${month}/${day}/${year}`);
-//   return `${month}/${day}/${year}`;
-// }
-
-// const format_time = (date) => {
-//   let idk = date.toLocaleTimeString();
-//   console.log(idk);
-//     return date.toLocaleTimeString();
-//   }
-
-// format_date('Sun Sep 03 2023 22:53:05 GMT-0500 (Central Daylight Time)')
-// format_time('Sun Sep 03 2023 22:53:05 GMT-0500 (Central Daylight Time)')
